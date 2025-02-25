0653bd5e8d4b89767736f8f81bb35a19495d3725
JoaquinR <55899656+JoaquinR98@users.noreply.github.com>
2025-02-25 18:14:03 +0100

    added cursor light effect + minor tweaks

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cursorLight = document.querySelector(".cursor-light");
+
+  let timeout;
+  let isMoving = false;
+
+  document.addEventListener("mousemove", (e) => {
+    cursorLight.style.left = `${e.clientX}px`;
+    cursorLight.style.top = `${e.clientY}px`;
+
+    cursorLight.style.opacity = "1";
+
+    // If the mouse is moving, reset the timeout to fade out the cursor light later
+    // if (!isMoving) {
+    //   isMoving = true;
+    //   clearTimeout(timeout);
+    // }
+
+    // timeout = setTimeout(() => {
+    //   cursorLight.style.opacity = "1";
+    //   isMoving = false;
+    // }, 300);
+  });
+});
